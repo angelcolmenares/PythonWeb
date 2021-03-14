56cba52552aecebeaf4851878dbc267b6068ee9a
angelcolmenares <angel.ignacio.colmenares@gmail.com>
2021-03-14 16:50:35 -0500

    create model: use config file

--- TornadoServer/main_http_server.py
+++ TornadoServer/main_http_server.py
@@ -6,14 +6,12 @@ import model_one
 
 class MainHandler(tornado.web.RequestHandler):
     
-    def initialize(self, model_path):
-        if not self.__is_model_initialized():
-            self.model = model_one.create_model_one(model_path)
-            self.__set_model_as_initialized()
+    def initialize(self, model):
+        self.__model= model
 
     def get(self):                       
         input_array = self.__get_input_array()
-        result = self.model.predict(input_array)
+        result = self.__model.predict(input_array)
         self.write({'prediction': str(result[0][0])})
 
     def __get_input_array(self):
@@ -25,21 +23,14 @@ class MainHandler(tornado.web.RequestHandler):
         input_six = float( self.get_argument("inputSix"))
         input_seven = float( self.get_argument("inputSeven"))
         input_eigth = float( self.get_argument("inputEigth"))
-        return [input_one,input_two, input_three, input_four,input_five,input_six,input_seven,input_eigth]
+        return [input_one,input_two, input_three, input_four,input_five,input_six,input_seven,input_eigth]    
 
-    def __is_model_initialized(self):
-        return  hasattr(self, "__model_initialized__") and self.__model_initialized__
-    
-    def __set_model_as_initialized(self):
-        self.__model_initialized__= True
-
-
-def make_app(config_file):    
-    url_ext = r"/predict" 
+def make_app(config_file): 
+    model = model_one.create_model_one(config_file)
+    url_ext = r"/predict"
     return tornado.web.Application([
-        (url_ext, MainHandler, dict(model_path="gmm0.9.hdf5")),
+        (url_ext, MainHandler, dict(model=model)),
     ])
-
 
 
 def get_args(argv):
@@ -49,32 +40,36 @@ def get_args(argv):
     try:
         opts, args = getopt.getopt(argv,"hp:c:",["port=", "--config-file"])
     except getopt.GetoptError:
-        print('main_http_server.py -p <port>')
-        sys.exit(2)
+        sys_exit('', 1)        
     for opt, arg in opts:
         if opt == '-h':
-            print( 'main_http_server.py -p <port>')
-            sys.exit()
+            sys_exit('', 0)            
         elif opt in ("-p", "--port"):
             port = arg            
         elif opt in ("-c", "--config-file"):
             config_file= arg
+
     if port=='':
-        print('main_http_server.py -p <port>')
-        sys.exit(2)                   
+        sys_exit("must indicate port number", 2)        
+    if( config_file==''):
+        sys_exit("must indicate config file", 3)    
     try:
         portAsInt = int(port)
     except :
-        print('invalid port number', port)
-        print('main_http_server.py -p <port>')
-        sys.exit(2)
+        sys_exit('invalid port number {}'.format(port),4)        
     return {'port':portAsInt, 'config_file':config_file}    
+
+def sys_exit(message='', exit_code=0):    
+    if message!='':
+        print(message)
+    usage = 'main_http_server.py -p <port> -c <config-file>'        
+    print(usage)
+    sys.exit(exit_code)
 
 
 def main(argv):
     app_args = get_args(argv)
-    print('config file ', app_args["config_file"] )
-    print ('listen on port ',  app_args["port"])
+    print('config-file:{config_file}. port:{port}'.format( config_file=app_args["config_file"], port=app_args["port"]))
     app = make_app(app_args["config_file"])
     app.listen( app_args["port"] )
     print ('listening...')
--- TornadoServer/model_one.py
+++ TornadoServer/model_one.py
@@ -6,26 +6,30 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import json 
 import numpy  as np
 from random import random,seed
+from keras.models  import Sequential,load_model
+
 seed(1)
 
 class ModelOne():
 
-    def __init__(self, model_path):        
-        from keras.models  import Sequential,load_model
-        self.model   = load_model(model_path)        
+    def __init__(self, config_file):
+        
+        with open(config_file) as json_file:
+            data_config = json.load(json_file)
+            model_path  = data_config["config"]["model_path"]
+            self.__model   = load_model(model_path)        
 
     def predict(self, input):        
         np_input = np.array(input)
-        prediction = self.model.predict(np_input.reshape(1,8))        
+        prediction = self.__model.predict(np_input.reshape(1,8))        
         return prediction
 
-def create_model_one(model_path):
-    return  ModelOne(model_path)
+def create_model_one(config_file):
+    return  ModelOne(config_file)
 
 
-if __name__ == '__main__':
-    print( get_version())
-    model = ModelOne('gmm0.9.hdf5')
+if __name__ == '__main__':    
+    model = ModelOne('config.json')
     prediction = model.predict([1,2,3,4,5,6,7,8])
     print(prediction)
     
